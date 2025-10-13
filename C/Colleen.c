c02d2e660fade097834d75bebfe8f35c3732a6f7
love <tlove.tky@gmail.com>
2025-10-13 11:35:17 +0900

    C - Colleenの実装

--- C/Colleen.c
+++ C/Colleen.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
-void Colleen() {
-    printf("Colleen\n");
+/*
+    Colleen
+*/
+
+void quote(void)
+{
+    char *code = "#include <stdio.h>%1$c%1$c/*%1$c    Colleen%1$c*/%1$c%1$cvoid quote(void)%1$c{%1$c    char *code = %2$c%s%2$c;%1$c    printf(code, 10, 34, code);%1$c}%1$c%1$cint main(void)%1$c{%1$c    /* main */%1$c    quote();%1$c    return 0;%1$c}%1$c";
+    printf(code, 10, 34, code);
 }
 
-int main() {
-    Colleen();
+int main(void)
+{
+    /* main */
+    quote();
     return 0;
 }
